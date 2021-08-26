841a82b1e68cca74cc0b22804fbda075e3e4af4a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-26 14:30:08 +0900

    post 요청 받기

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8000, () => {
     console.log('8080 port');
@@ -8,3 +10,12 @@ app.listen(8000, () => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/write', (req, res) => {
+    res.sendFile(__dirname + '/write.html');
+})
+
+app.post('/add', (req,res) => {
+    res.send('<h1>전송 완료</h1>');
+    console.log(req.body.title)
+})
