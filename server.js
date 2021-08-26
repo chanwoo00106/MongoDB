02662279e8fbbd126d06a33b72d6a5727d9faf18
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-26 22:38:02 +0900

    Add list.ejs

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const MongoClient = require('mongodb').MongoClient;
 
+app.set('view engine', 'ejs');
+
 MongoClient.connect(
     'mongodb+srv://chanwoo:<password>@cluster0.jvys6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     (error, client) => {
         if (error) return console.log(error);
+        db = client.db('todoapp');
+
         app.listen(8000, () => {
             console.log('8080 port');
+            
         });
     }
 );
@@ -21,9 +26,19 @@ app.get('/', (req, res) => {
 
 app.get('/write', (req, res) => {
     res.sendFile(__dirname + '/write.html');
-})
+});
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray((error, result) => {
+        if (error) console.error(error);
+        console.log(result);
+        res.render('list.ejs', {posts: result});
+    });
+});
 
 app.post('/add', (req,res) => {
     res.send('<h1>전송 완료</h1>');
-    console.log(req.body);
+    db.collection('post').insertOne({title: req.body.title, date: req.body.date}, (error, result) => {
+        console.log('완료')
+    });
 })
