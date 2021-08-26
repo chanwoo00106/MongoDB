e750fd26aebe33ed293802cfbf6b10735db82963
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-26 20:36:27 +0900

    mongoDB 연결

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(8000, () => {
-    console.log('8080 port');
-});
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(
+    'mongodb+srv://chanwoo:<password>@cluster0.jvys6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    (error, client) => {
+        if (error) return console.log(error);
+        app.listen(8000, () => {
+            console.log('8080 port');
+        });
+    }
+);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,5 +25,5 @@ app.get('/write', (req, res) => {
 
 app.post('/add', (req,res) => {
     res.send('<h1>전송 완료</h1>');
-    console.log(req.body.title)
+    console.log(req.body);
 })
